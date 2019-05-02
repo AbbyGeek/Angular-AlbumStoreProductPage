d85a98512c287b955e1167aeb89f1faaea3c22c2
Wessels (Secondary) <AWessels-@quickenloans.com>
2019-05-02 14:29:23 -0400

    fixed typo

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,11 +8,11 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
   albumInfo;
-  constructor( private _productService) { 
+  constructor( private _productService : ProductService) { 
   }
 
   ngOnInit() {
-    this._productService.getAlbun(1).subscribe(response => this.albumInfo = response);
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
